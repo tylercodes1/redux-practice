2cffa44730b4809ed9cb6e31339091658ccfed3f
tkmoney11 <kimt29@uw.edu>
2021-07-20 23:20:54 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TestPage from './pages/TestPage';
+
+function App() {
+  return (
+    <div className="App">
+      <TestPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/TestPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './TestPage.css'
+
+const TestPage = () => { 
+    return (
+        <div className="test-page">Test Page</div>
+    )
+}
+
+export default TestPage
